d6c62018836f2dc490443302facd687eb34db99b
Daniyar Kaiyrbolatov <daniyarkayirbolatov@gmail.com>
2018-07-21 00:29:40 +0500

    menu bar closed at first open

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	$("#nav-menu-left").click(function() {
 		console.log('Clicked!');
-		$("#nav-left").toggle('slide');
 		if (!$("#menuToggle").hasClass('btn-close')) {
 			$("#menuToggle").addClass('btn-close');
+			$("#nav-left").removeClass('closed');
+			$("#nav-left-list").removeClass('closed');
 		} else {
 			$("#menuToggle").removeClass('btn-close');
+			$("#nav-left").addClass('closed');
+			$("#nav-left-list").addClass('closed');
 		}
 		
 	});
